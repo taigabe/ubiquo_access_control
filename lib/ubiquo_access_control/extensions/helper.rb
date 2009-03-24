c763f7df5d955857be7cb446130fbababc534f4e
Albert <acallarisa@desktop24.gnuine.loc>
2009-03-24 09:58:14 +0100

    Added permissions management helper method used in ubiquo_user edit form

--- lib/ubiquo_access_control/extensions/helper.rb
+++ lib/ubiquo_access_control/extensions/helper.rb
@@ -8,6 +8,19 @@ module UbiquoAccessControl
           link.url = ubiquo_roles_path
         end if ubiquo_config_call(:role_permit, {:context => :ubiquo_access_control})
       end
+      
+      def user_permission_fields(form)
+        ubiquo_user_roles = form.object.ubiquo_user_roles.map(&:role)
+        content_tag("h3", t("ubiquo.auth.roles")) + content_tag("ul", :class => "permissions") do 
+          @roles.map do |role|
+            content_tag("li") do 
+              check_box_tag("ubiquo_user[role_ids][]", role.id, ubiquo_user_roles.include?(role), {:id => "role"+role.id.to_s}) +
+                label_tag("role"+role.id.to_s, role.name)
+              
+            end
+          end
+        end
+      end
     end
   end
 end
