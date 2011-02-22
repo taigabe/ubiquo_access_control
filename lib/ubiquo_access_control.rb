90b6b8416112e91b378008f16936162da0fae567
Ramon Salvadó <rsalvado@gnuine.com>
2011-02-22 09:41:29 +0100

    Removed obsolete requires and autoload, load testcase improvements only when needed [#457 state:updated]

--- lib/ubiquo_access_control.rb
+++ lib/ubiquo_access_control.rb
@@ -1,5 +1,3 @@
-require 'ubiquo_access_control/access_control.rb'
 require 'ubiquo_access_control/extensions.rb'
-require 'ubiquo_access_control/version.rb'
 
 Ubiquo::Extensions::UbiquoAreaController.append_include(UbiquoAccessControl::AccessControl)
