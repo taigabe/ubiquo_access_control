fd3b235877ebd39bae1b49d848824cf8a6a7a1f4
Toni Reina <areina0@gmail.com>
2012-01-16 11:05:25 +0100

    Changed rails/init.rb for init_settings initializer

--- lib/ubiquo_access_control.rb
+++ lib/ubiquo_access_control.rb
@@ -1,11 +1,17 @@
+require 'ubiquo'
+
 module UbiquoAccessControl
   class Engine < Rails::Engine
-    config.paths["lib"].autoload!
+    include Ubiquo::Engine::Base
 
     initializer :load_extensions do
       require 'ubiquo_access_control/extensions.rb'
 
       Ubiquo::Extensions::Loader.append_include(:UbiquoController, UbiquoAccessControl::AccessControl)
+    end
+
+    initializer :register_ubiquo_plugin do
+      require 'ubiquo_access_control/init_settings.rb'
     end
 
   end
---
+++ lib/ubiquo_access_control/init_settings.rb
@@ -0,0 +1,18 @@
+module UbiquoAccessControl
+  module Config #:nodoc:
+    Ubiquo::Plugin.register(:ubiquo_access_control) do |config|
+      config.add :role_access_control, lambda{
+        access_control :DEFAULT => "role_management"
+      }
+      config.add :role_permit, lambda{
+        permit?("role_management")
+      }
+
+      config.add :roles_elements_per_page
+      config.add_inheritance :roles_elements_per_page, :elements_per_page
+
+      config.add :roles_default_order_field, 'id'
+      config.add :roles_default_sort_order, 'desc'
+    end
+  end
+end
